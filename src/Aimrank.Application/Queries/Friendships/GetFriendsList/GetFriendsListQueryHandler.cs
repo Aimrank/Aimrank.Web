2d3d781686bd016892b53081033aca172e1b33c1
Mariusz Baran <mariusz.baran23@gmail.com>
2021-03-04 18:00:33 +0100

    Write SQL for friendship query handlers

--- src/Aimrank.Application/Queries/Friendships/GetFriendsList/GetFriendsListQueryHandler.cs
+++ src/Aimrank.Application/Queries/Friendships/GetFriendsList/GetFriendsListQueryHandler.cs
@@ -1,5 +1,7 @@
 using Aimrank.Application.Contracts;
 using Aimrank.Application.Queries.Users.GetUserDetails;
+using Aimrank.Common.Application.Data;
+using Dapper;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading;
@@ -8,9 +10,41 @@ namespace Aimrank.Application.Queries.Friendships.GetFriendsList
 {
     internal class GetFriendsListQueryHandler : IQueryHandler<GetFriendsListQuery, IEnumerable<UserDto>>
     {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public GetFriendsListQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
         public Task<IEnumerable<UserDto>> Handle(GetFriendsListQuery request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var connection = _sqlConnectionFactory.GetOpenConnection();
+
+            const string sql = @"
+                SELECT
+                    CASE
+                        WHEN [U1].[Id] = @UserId THEN [U2].[Id]
+                        WHEN [U2].[Id] = @UserId THEN [U1].[Id]
+                    END AS [Id],
+                    CASE
+                        WHEN [U1].[Id] = @UserId THEN [U2].[UserName]
+                        WHEN [U2].[Id] = @UserId THEN [U1].[UserName]
+                    END AS [Username],
+                    CASE
+                        WHEN [U1].[Id] = @UserId THEN [U2].[SteamId]
+                        WHEN [U2].[Id] = @UserId THEN [U1].[SteamId]
+                    END AS [SteamId]
+                FROM [aimrank].[Friendships] AS [F]
+                INNER JOIN [aimrank].[AspNetUsers] AS [U1] ON [U1].[Id] = [F].[User1Id]
+                INNER JOIN [aimrank].[AspNetUsers] AS [U2] ON [U2].[Id] = [F].[User2Id]
+                WHERE
+                    [F].[IsAccepted] = 1 AND
+                    [F].[BlockingUserId1] IS NULL AND
+                    [F].[BlockingUserId2] IS NULL AND
+                    ([U1].[Id] = @UserId OR [U2].[Id] = @UserId);";
+
+            return connection.QueryAsync<UserDto>(sql, new {request.UserId});
         }
     }
 }
--- src/Aimrank.Application/Queries/Friendships/GetFriendshipInvitations/GetFriendshipInvitationsQueryHandler.cs
+++ src/Aimrank.Application/Queries/Friendships/GetFriendshipInvitations/GetFriendshipInvitationsQueryHandler.cs
@@ -1,17 +1,50 @@
 using Aimrank.Application.Contracts;
 using Aimrank.Application.Queries.Users.GetUserDetails;
+using Aimrank.Common.Application.Data;
+using Dapper;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading;
-using System;
 
 namespace Aimrank.Application.Queries.Friendships.GetFriendshipInvitations
 {
     internal class GetFriendshipInvitationsQueryHandler : IQueryHandler<GetFriendshipInvitationsQuery, IEnumerable<UserDto>>
     {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public GetFriendshipInvitationsQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
         public Task<IEnumerable<UserDto>> Handle(GetFriendshipInvitationsQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var connection = _sqlConnectionFactory.GetOpenConnection();
+            
+            const string sql = @"
+                SELECT
+                    CASE
+                        WHEN [U1].[Id] = @UserId THEN [U2].[Id]
+                        WHEN [U2].[Id] = @UserId THEN [U1].[Id]
+                    END AS [Id],
+                    CASE
+                        WHEN [U1].[Id] = @UserId THEN [U2].[UserName]
+                        WHEN [U2].[Id] = @UserId THEN [U1].[UserName]
+                    END AS [Username],
+                    CASE
+                        WHEN [U1].[Id] = @UserId THEN [U2].[SteamId]
+                        WHEN [U2].[Id] = @UserId THEN [U1].[SteamId]
+                    END AS [SteamId]
+                FROM [aimrank].[Friendships] AS [F]
+                INNER JOIN [aimrank].[AspNetUsers] AS [U1] ON [U1].[Id] = [F].[User1Id]
+                INNER JOIN [aimrank].[AspNetUsers] AS [U2] ON [U2].[Id] = [F].[User2Id]
+                WHERE
+                    [F].[IsAccepted] = 0 AND
+                    [F].[BlockingUserId1] IS NULL AND
+                    [F].[BlockingUserId2] IS NULL AND
+                    ([U1].[Id] = @UserId OR [U2].[Id] = @UserId);";
+
+            return connection.QueryAsync<UserDto>(sql, new {request.UserId});
         }
     }
 }
