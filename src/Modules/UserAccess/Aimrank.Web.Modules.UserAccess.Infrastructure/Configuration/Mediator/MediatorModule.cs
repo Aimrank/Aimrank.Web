407ef87edea62365ebf71a4517fc0bd56a2257ff
Mariusz Baran <mariusz.baran23@gmail.com>
2021-04-19 23:10:42 +0200

    Refactor MediatR DI registrations

--- src/Modules/UserAccess/Aimrank.Web.Modules.UserAccess.Infrastructure/Configuration/Mediator/MediatorModule.cs
+++ src/Modules/UserAccess/Aimrank.Web.Modules.UserAccess.Infrastructure/Configuration/Mediator/MediatorModule.cs
@@ -1,12 +1,7 @@
-using Autofac.Core;
-using Autofac.Features.Variance;
 using Autofac;
 using FluentValidation;
+using MediatR.Extensions.Autofac.DependencyInjection;
 using MediatR;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System;
 
 namespace Aimrank.Web.Modules.UserAccess.Infrastructure.Configuration.Mediator
 {
@@ -14,80 +9,15 @@ namespace Aimrank.Web.Modules.UserAccess.Infrastructure.Configuration.Mediator
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly)
-                .AsImplementedInterfaces()
-                .InstancePerLifetimeScope();
-
-            builder.RegisterSource(new ScopedContravariantRegistrationSource(
-                typeof(IRequestHandler<,>),
-                typeof(IRequestHandler<>),
-                typeof(INotificationHandler<>),
-                typeof(IValidator<>)));
-
-            var mediatorOpenTypes = new[]
-            {
-                typeof(IRequestHandler<,>),
-                typeof(IRequestHandler<>),
-                typeof(INotificationHandler<>),
-                typeof(IValidator<>)
-            };
-
-            foreach (var mediatorOpenType in mediatorOpenTypes)
-            {
-                builder
-                    .RegisterAssemblyTypes(ThisAssembly, Assemblies.Application)
-                    .AsClosedTypesOf(mediatorOpenType)
-                    .AsImplementedInterfaces();
-            }
-
+            builder.RegisterAssemblyTypes(Assemblies.Application, Assemblies.Infrastructure)
+                .AsClosedTypesOf(typeof(IValidator<>))
+                .SingleInstance();
+            
+            builder.RegisterMediatR(Assemblies.Application, Assemblies.Infrastructure);
+            
             builder.RegisterGeneric(typeof(LoggingPipelineBehavior<,>)).As(typeof(IPipelineBehavior<,>));
             builder.RegisterGeneric(typeof(ValidationPipelineBehavior<,>)).As(typeof(IPipelineBehavior<,>));
             builder.RegisterGeneric(typeof(UnitOfWorkPipelineBehavior<,>)).As(typeof(IPipelineBehavior<,>));
-
-            builder.Register<ServiceFactory>(ctx =>
-            {
-                var c = ctx.Resolve<IComponentContext>();
-                return t => c.Resolve(t);
-            }).InstancePerLifetimeScope();
-        }
-        
-        private class ScopedContravariantRegistrationSource : IRegistrationSource
-        {
-            private readonly IRegistrationSource _source = new ContravariantRegistrationSource();
-            private readonly List<Type> _types = new List<Type>();
-
-            public ScopedContravariantRegistrationSource(params Type[] types)
-            {
-                if (types is null)
-                {
-                    throw new ArgumentNullException(nameof(types));
-                }
-
-                if (!types.All(x => x.IsGenericTypeDefinition))
-                {
-                    throw new ArgumentException("Supplied types should be generic type definitions");
-                }
-                
-                _types.AddRange(types);
-            }
-
-            public IEnumerable<IComponentRegistration> RegistrationsFor(Service service, Func<Service, IEnumerable<ServiceRegistration>> registrationAccessor)
-            {
-                var components = _source.RegistrationsFor(service, registrationAccessor);
-                foreach (var c in components)
-                {
-                    var defs = c.Target.Services
-                        .OfType<TypedService>()
-                        .Select(x => x.ServiceType.GetGenericTypeDefinition());
-
-                    if (defs.Any(_types.Contains))
-                    {
-                        yield return c;
-                    }
-                }
-            }
-
-            public bool IsAdapterForIndividualComponents => _source.IsAdapterForIndividualComponents;
         }
     }
 }
