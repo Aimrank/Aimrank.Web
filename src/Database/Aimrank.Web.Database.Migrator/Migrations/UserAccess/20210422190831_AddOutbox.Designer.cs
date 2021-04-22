d112f1958276e27a6b32b43d1691da50aecdde95
Mariusz Baran <mariusz.baran23@gmail.com>
2021-04-22 21:10:35 +0200

    Add UserAccess module migrations

--- src/Database/Aimrank.Web.Database.Migrator/Migrations/UserAccess/20210422190831_AddOutbox.Designer.cs
+++ src/Database/Aimrank.Web.Database.Migrator/Migrations/UserAccess/20210422190831_AddOutbox.Designer.cs
@@ -4,14 +4,16 @@ using Aimrank.Web.Modules.UserAccess.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Aimrank.Web.Database.Migrator.Migrations.UserAccess
 {
     [DbContext(typeof(UserAccessContext))]
-    partial class UserAccessContextModelSnapshot : ModelSnapshot
+    [Migration("20210422190831_AddOutbox")]
+    partial class AddOutbox
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Modules/UserAccess/Aimrank.Web.Modules.UserAccess.Infrastructure/Configuration/Processing/Outbox/OutboxMessage.cs
+++ src/Modules/UserAccess/Aimrank.Web.Modules.UserAccess.Infrastructure/Configuration/Processing/Outbox/OutboxMessage.cs
@@ -6,7 +6,6 @@ namespace Aimrank.Web.Modules.UserAccess.Infrastructure.Configuration.Processing
     {
         public Guid Id { get; }
         public DateTime OccurredAt { get; }
-        public DateTime ProcessedDate { get; }
         public string Type { get; }
         public string Data { get; }
 
